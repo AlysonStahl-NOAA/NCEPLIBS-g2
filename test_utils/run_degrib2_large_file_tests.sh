d83d10b8610197dfceca77f3e0837c207b8ba8e7
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-06-04 11:43:33 -0600

    more debugging

--- test_utils/run_degrib2_large_file_tests.sh
+++ test_utils/run_degrib2_large_file_tests.sh
@@ -17,7 +17,7 @@ ftp_files="fv3lam.t00z.prslev.f000.grib2"
 for f in $ftp_files
 do
     echo "Testing degrib2 with large file $f"
-    ls -l data/$f
+    ls -l ../tests/data/$f
     ../utils/degrib2 ../tests/data/$f > ${f}.degrib2
     diff -w ${f}.degrib2 data/ref_${f}.degrib2
 done
